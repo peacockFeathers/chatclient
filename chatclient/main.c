34983a73facd061750e30e3df0d2a41dce9135da
Justin Peacock <peacockj@oregonstate.edu>
2016-02-05 21:48:54 -0800

    Initial Commit

---
+++ chatclient/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  chatclient
+//
+//  Created by Justin Peacock on 2/5/16.
+//  Copyright © 2016 Justin Peacock. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
